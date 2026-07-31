5c70ff7cc3e7c2c2c264d70efdc12312ee365a8f
Kelly <kelkytan2002@gmail.com>
2026-07-31 02:29:50 -0700

    value terbalik

--- main.cpp
+++ main.cpp
@@ -85,8 +85,8 @@ int main() {
         std::vector<Portal> portals = {
             { {1.0f, 1.0f}, {1.0f, 0.5f} },
             { {2.0f, 1.0f}, {2.0f, 0.5f} },
-            { {3.0f, -1.0f}, {3.0f, -0.5f} },
-            { {4.0f, -1.0f}, {4.0f, -0.5f} },
+            { {3.0f, -0.5f}, {3.0f, -1.0f} },
+            { {4.0f, -0.5f}, {4.0f, -1.0f} },
             { {5.0f, 0.5f}, {5.0f, 0.0f} },
         };
         allResults.push_back(RunCorridor("Sharp zigzag (S-curve)", start, goal, portals));
